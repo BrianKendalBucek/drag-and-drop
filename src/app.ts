483c274460d6aaac6051a6a839fa20714dec063e
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-25 21:23:40 -0700

    added bind to clarify which this were referring to

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,17 @@ class ProjectInput {
       "#people"
     ) as HTMLInputElement;
 
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this))
   }
 
   private attach() {

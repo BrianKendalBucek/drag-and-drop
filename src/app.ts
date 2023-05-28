48f79cdcb64c26a554e2670d262e05cac08f18cb
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-05-28 13:50:20 -0700

    fifth check in validate function complete

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,12 @@ function validate(validatableInput: Validatable) {
     isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
 
   }
+  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value > validatableInput.min;
+  }
+  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
   return isValid;
 }
 

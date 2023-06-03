bb50dddd07d6c86a6033490182c0f3a1f4116ec8
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-02 19:35:02 -0700

    added querySelectors to renderContent

--- src/app.ts
+++ src/app.ts
@@ -170,7 +170,9 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   configure() {}
 
   renderContent() {
-    
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
 

11e3497ac61052b45c9da97b5cb7351820a76590
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-06-04 08:16:09 -0700

    adding Draggable and DragTarget interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
+// Drag & Drop Interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {}
+
 // Project Type
 enum ProjectStatus {
   Active,
